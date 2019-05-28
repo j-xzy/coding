56a739d47a18e922ef60e86ff4acb3d3048b8a74
whj1995 <wu38607@live.com>
2019-05-28 23:27:07 +0800

    .

---
+++ 1.两数之和.js
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=javascript
+ *
+ * [1] 两数之和
+ */
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    
+};
+
---
+++ 48.旋转图像.js
@@ -0,0 +1,13 @@
+/*
+ * @lc app=leetcode.cn id=48 lang=javascript
+ *
+ * [48] 旋转图像
+ */
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function(matrix) {
+  
+};
+
