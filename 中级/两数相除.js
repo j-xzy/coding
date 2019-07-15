0dee1ec5679811fe230d0b20d07d56aff0e5b9b4
whj1995 <wu38607@live.com>
2019-07-15 01:19:03 +0000

    .

--- 中级/两数相除.js
+++ 中级/两数相除.js
@@ -16,9 +16,12 @@
  * 假设我们的环境只能存储 32 位有符号整数，其数值范围是 [−231,  231 − 1]。本题中，如果除法结果溢出，则返回 231 − 1。
  */
 
-// 100 / 30 = 3.3333
-// 30 + 30 + 30
-// 
+// 5 10 20 50 80
+// 5 5  5  5  5
+// 1 2  4  10  16
+// 5 10 20 40 80
+// 8 + 10
+// 1 2 4 8 16
 
 /**
  * @param {number} dividend
@@ -38,34 +41,40 @@ var divide = function (dividend, divisor) {
   if (divisor === -1) {
     return over(0 - dividend);
   }
-  const a = Math.abs(dividend);
-  const b = Math.abs(divisor);
-  if (b > a) {
+  const absDividend = Math.abs(dividend);
+  const asbdivisor = Math.abs(divisor);
+  if (asbdivisor > absDividend) {
     return 0;
   }
-  let c = 0;
-  let r = b;
-  while (true) {
-    ++c;
-    r += b;
-    if (r === a) {
-      ++c;
-      break;
+
+  let count = 0;
+  let rest = absDividend;
+  
+  while (rest > 0) {
+    let num = lastNum = asbdivisor;
+    let innerCount = lastInnerCount = 1;
+    while (true) {
+      innerCount += innerCount;
+      num += num;
+      if (num > rest) {
+        rest = rest - lastNum;
+        innerCount = lastInnerCount;
+        if(rest < 0) {
+          --innerCount;
+        }
+        break;
+      }
+      lastInnerCount += lastInnerCount;
+      lastNum += lastNum;
     }
-    if (r > a) {
-      break;
-    }
+    count += innerCount;
   }
 
-  const aIsAbs = dividend > 0;
-  const bIsAbs = divisor > 0;
-  if (aIsAbs && bIsAbs) {
-    return over(c);
+  if ((dividend ^ divisor) > 0) {
+    return over(count);
+  } else {
+    return over(0 - count);
   }
-  if (!aIsAbs && !bIsAbs) {
-    return over(c);
-  }
-  return over(0 - c);
 };
 
 function over(num) {
@@ -75,8 +84,8 @@ function over(num) {
   return num;
 }
 
-// console.log(divide(-2147483648, -1));
+console.log(divide(2147483647, 2));
+console.log(divide(-2147483648, -1));
 console.log(divide(7, -3));
-console.log(divide(12, 5));
 console.log(divide(15, 5));
 console.log(divide(100, 3));
