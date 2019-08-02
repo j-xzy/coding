8d2b64f89819aa8ccc02312a079a1e02866d84bf
whj1995 <wu38607@live.com>
2019-08-02 01:42:19 +0000

    .

---
+++ 链表/反转链表.js
@@ -0,0 +1,51 @@
+/**
+ * 反转一个单链表。
+ * 示例:
+ * 输入: 1->2->3->4->5->NULL
+ * 输出: 5->4->3->2->1->NULL
+ * 
+ * 进阶:
+ * 你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+ */
+
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+  if (head === null) {
+    return null;
+  }
+  if (head.next === null) {
+    return head;
+  }
+  let cur = null;
+  let next = head;
+  while (next) {
+    const temp = next.next;
+    next.next = cur;
+    cur = next;
+    next = temp;
+  }
+
+  return cur;
+};
+
+const node1 = new ListNode(1);
+const node2 = new ListNode(2);
+const node3 = new ListNode(3);
+node1.next = node2;
+node2.next = node3;
+reverseList(node1);
