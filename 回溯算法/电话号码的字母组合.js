3506d048816bea3a3082863467c7e71058dc4c95
whj1995 <wu38607@live.com>
2019-08-13 03:04:30 +0000

    .

---
+++ 回溯算法/电话号码的字母组合.js
@@ -0,0 +1,46 @@
+/**
+ * 给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。
+ * 给出数字到字母的映射如下（与电话按键相同）。注意1不对应任何字母。
+ * 
+ * 示例:
+ * 输入："23"
+ * 输出：["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+ * 说明:
+ * 尽管上面的答案是按字典序排列的，但是你可以任意选择答案输出的顺序。
+ */
+
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function (digits) {
+  if (digits.length === 0) {
+    return [];
+  }
+  const map = {
+    '2': 'abc',
+    '3': 'def',
+    '4': 'ghi',
+    '5': 'jkl',
+    '6': 'mno',
+    '7': 'pqrs',
+    '8': 'tuv',
+    '9': 'wxyz'
+  };
+  const result = [];
+  combination('', 0);
+  return result;
+
+  function combination(str, idx) {
+    if (idx >= digits.length) {
+      result.push(str);
+      return;
+    }
+    const c = map[digits[idx]];
+    for (let i = 0; i < c.length; ++i) {
+      combination(str + c[i], idx + 1);
+    }
+  }
+};
+
+letterCombinations('23');
