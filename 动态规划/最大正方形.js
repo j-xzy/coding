6be0885073b0671990c8097ded085bac9ad8d82f
whj1995 <wu38607@live.com>
2019-09-08 05:24:03 +0000

    .

---
+++ 动态规划/最大正方形.js
@@ -0,0 +1,56 @@
+/**
+ * 在一个由 0 和 1 组成的二维矩阵内，找到只包含 1 的最大正方形，并返回其面积。
+ * 
+ * 示例:
+ * 输入: 
+ * 1 0 1 0 0
+ * 1 0 1 1 1
+ * 1 1 1 1 1
+ * 1 0 0 1 0
+ * 输出: 4
+ */
+
+// dp[r][c]
+
+/**
+ * @param {character[][]} matrix
+ * @return {number}
+ */
+var maximalSquare = function (matrix) {
+  const rowCount = matrix.length;
+  if (rowCount === 0) {
+    return 0;
+  }
+  const colCount = matrix[0].length;
+  let max = 0;
+  const dp = [...Array(rowCount)].map(() => Array(colCount).fill(0));
+  for (let r = 0; r < rowCount; ++r) {
+    for (let c = 0; c < colCount; ++c) {
+      if (matrix[r][c] === '0') {
+        continue;
+      }
+      if (r === 0 || c === 0) {
+        dp[r][c] = 1;
+      } else if (dp[r - 1][c] === 0 || dp[r][c - 1] === 0) {
+        dp[r][c] = 1;
+      } else if (dp[r - 1][c] === dp[r][c - 1]) {
+        const step = dp[r - 1][c];
+        if( matrix[r - step][c - step] === '1') {
+          dp[r][c] = step + 1;
+        } else {
+          dp[r][c] = step;
+        }
+      } else {
+        dp[r][c] = Math.min(dp[r - 1][c], dp[r][c - 1]) + 1;
+      }
+      max = Math.max(max,dp[r][c]);
+    }
+  }
+  return max * max;
+};
+
+maximalSquare([
+  ["1","0","1","0","0"],
+  ["1","0","1","1","1"],
+  ["1","1","1","1","1"],
+  ["1","0","0","1","0"]])
---
+++ 动态规划/组合总和Ⅳ.js
@@ -0,0 +1,49 @@
+/**
+ * 给定一个由正整数组成且不存在重复数字的数组，找出和为给定目标正整数的组合的个数。
+ * 
+ * 示例:
+ * nums = [1, 2, 3]
+ * target = 4
+ * 所有可能的组合为：
+ * (1, 1, 1, 1)
+ * (1, 1, 2)
+ * (1, 2, 1)
+ * (1, 3)
+ * (2, 1, 1)
+ * (2, 2)
+ * (3, 1)
+ * 请注意，顺序不同的序列被视作不同的组合。
+ * 因此输出为 7。
+ */
+
+const memo = {};
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var combinationSum4 = function (nums, target) {
+  if (target < 1) {
+    return 0;
+  }
+  if (nums.length === 0) {
+    return 0;
+  }
+  if (memo[target] !== undefined) {
+    return memo[target];
+  }
+  let count = 0;
+  for (let i = 0; i < nums.length; ++i) {
+    const rest = target - nums[i];
+    if (rest === 0) {
+      count += 1;
+    } else {
+      count += combinationSum4(nums, rest);
+    }
+  }
+  memo[target] = count;
+  return count;
+};
+
+console.log(combinationSum4([9], 3))
