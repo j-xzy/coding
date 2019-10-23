772e053cba545040933a2180ec31d57f02c82f22
whj1995 <wu38607@live.com>
2019-10-23 02:53:29 +0000

    .

---
+++ 树/另一个树的子树.js
@@ -0,0 +1,80 @@
+/**
+给定两个非空二叉树 s 和 t，检验 s 中是否包含和 t 具有相同结构和节点值的子树。s 的一个子树包括 s 的一个节点和这个节点的所有子孙。s 也可以看做它自身的一棵子树。
+
+示例 1:
+给定的树 s:
+
+     3
+    / \
+   4   5
+  / \
+ 1   2
+给定的树 t：
+
+   4 
+  / \
+ 1   2
+返回 true，因为 t 与 s 的一个子树拥有相同的结构和节点值。
+
+示例 2:
+给定的树 s：
+
+     3
+    / \
+   4   5
+  / \
+ 1   2
+    /
+   0
+给定的树 t：
+
+   4
+  / \
+ 1   2
+返回 false。
+ */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} s
+ * @param {TreeNode} t
+ * @return {boolean}
+ */
+var isSubtree = function (s, t) {
+  const queue = [s];
+  while (queue.length > 0) {
+    const size = queue.length;
+    for (let i = 0; i < size; ++i) {
+      const node = queue.shift();
+      if(equal(node,t)) {
+        return true;
+      }
+      if (node.left) {
+        queue.push(node.left);
+      }
+      if (node.right) {
+        queue.push(node.right);
+      }
+    }
+  }
+  return false;
+};
+
+function equal(s, t) {
+  if (s === null && t === null) {
+    return true;
+  }
+  if (s === null || t === null) {
+    return false;
+  }
+  if (s.val === t.val) {
+    return equal(s.left, t.left) && equal(s.right, t.right) ;
+  }
+  return false;
+}
