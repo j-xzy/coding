e63747fab052025db21d53cc7e412e5e7be110af
whj1995 <wu38607@live.com>
2019-11-26 02:13:41 +0000

    .

---
+++ 树/路径总和III.js
@@ -0,0 +1,66 @@
+/**
+给定一个二叉树，它的每个结点都存放着一个整数值。
+
+找出路径和等于给定数值的路径总数。
+
+路径不需要从根节点开始，也不需要在叶子节点结束，但是路径方向必须是向下的（只能从父节点到子节点）。
+
+二叉树不超过1000个节点，且节点数值范围是 [-1000000,1000000] 的整数。
+
+示例：
+
+root = [10,5,-3,3,2,null,11,3,-2,null,1], sum = 8
+
+      10
+     /  \
+    5   -3
+   / \    \
+  3   2   11
+ / \   \
+3  -2   1
+
+返回 3。和等于 8 的路径有:
+
+1.  5 -> 3
+2.  5 -> 2 -> 1
+3.  -3 -> 11
+ */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} sum
+ * @return {number}
+ */
+var pathSum = function (root, sum) {
+  let ans = 0;
+  traverse(root);
+  return ans;
+
+  function traverse(node) {
+    if (!node) {
+      return;
+    }
+    count(node, sum);
+    traverse(node.left);
+    traverse(node.right);
+  }
+
+  function count(node, rest) {
+    if (!node) {
+      return;
+    }
+    const val = rest - node.val;
+    if (val === 0) {
+      ++ans;
+    }
+    count(node.left,val);
+    count(node.right, val);
+  }
+};
