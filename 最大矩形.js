b4a781017b4244900ed19c55cb958f79d363b7d1
whj1995 <wu38607@live.com>
2019-12-29 14:28:26 +0800

    .

---
+++ 最大矩形.js
@@ -0,0 +1,57 @@
+/**
+给定一个仅包含 0 和 1 的二维二进制矩阵，找出只包含 1 的最大矩形，并返回其面积。
+
+示例:
+输入:
+[
+  ["1","0","1","0","0"],
+  ["1","0","1","1","1"],
+  ["1","1","1","1","1"],
+  ["1","0","0","1","0"]
+]
+输出: 6
+ */
+
+// 题解：ttps://leetcode-cn.com/problems/maximal-rectangle/solution/zui-da-ju-xing-by-leetcode/ 
+
+// 思路：dp柱状图,方法2
+/**
+ * @param {character[][]} matrix
+ * @return {number}
+ */
+var maximalRectangle = function (matrix) {
+  const rowCount = matrix.length;
+  if (rowCount === 0) {
+    return 0;
+  }
+  let ans = 0;
+  const colCount = matrix[0].length;
+  const dp = [...Array(rowCount)].map(() => Array(colCount).fill(0));
+  for (let r = 0; r < rowCount; ++r) {
+    for (let c = 0; c < colCount; ++c) {
+      if (matrix[r][c] === '0') {
+        continue;
+      }
+      if (c === 0) {
+        dp[r][c] = 1;
+      } else {
+        dp[r][c] = dp[r][c - 1] + 1;
+      }
+      let width = dp[r][c];
+      for (let j = r; j >= 0; --j) {
+        width = Math.min(width, dp[j][c]);
+        let height = r - j + 1;
+        let area = width * height;
+        ans = Math.max(ans, area);
+      }
+    }
+  }
+  return ans;
+};
+
+maximalRectangle([
+  ["1", "0", "1", "0", "0"],
+  ["1", "0", "1", "1", "1"],
+  ["1", "1", "1", "1", "1"],
+  ["1", "0", "0", "1", "0"]
+])
---
+++ 柱状图中最大的矩形.js
@@ -0,0 +1,33 @@
+/**
+给定 n 个非负整数，用来表示柱状图中各个柱子的高度。每个柱子彼此相邻，且宽度为 1 。
+求在该柱状图中，能够勾勒出来的矩形的最大面积。
+以上是柱状图的示例，其中每个柱子的宽度为 1，给定的高度为 [2,1,5,6,2,3]。
+图中阴影部分为所能勾勒出的最大矩形面积，其面积为 10 个单位。
+
+示例:
+
+输入: [2,1,5,6,2,3]
+输出: 10
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/largest-rectangle-in-histogram
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+/**
+ * @param {number[]} heights
+ * @return {number}
+ */
+var largestRectangleArea = function (heights) {
+  let ans = 0;
+  for (let l = 0; l < heights.length; ++l) {
+    let height = Infinity;
+    for (let r = l; r < heights.length; ++r) {
+      height = Math.min(height, heights[r]);
+      ans = Math.max(ans, height * (r - l + 1));
+    }
+  }
+  return ans;
+};
+
+largestRectangleArea([2, 1, 5, 6, 2, 3]);
